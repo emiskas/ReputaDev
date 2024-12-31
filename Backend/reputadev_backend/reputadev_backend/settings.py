03ec46d9b0c63f0762a2f20066d962368e8ac05d
Emilis Paulauskas <emilispaulauskas279@gmail.com>
2024-12-31 11:47:53 +0200

    Integrate PostgreSQL database and migrate.

--- Backend/reputadev_backend/reputadev_backend/settings.py
+++ Backend/reputadev_backend/reputadev_backend/settings.py
@@ -88,13 +88,21 @@ WSGI_APPLICATION = "reputadev_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
