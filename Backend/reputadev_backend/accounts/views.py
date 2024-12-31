b73b0bb766a9a270527dd6758092924f01c211b6
Emilis Paulauskas <emilispaulauskas279@gmail.com>
2024-12-31 07:41:11 +0200

    Add views for registration and profile editing.

--- Backend/reputadev_backend/accounts/views.py
+++ Backend/reputadev_backend/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("login")
+    else:
+        form = UserRegistrationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileUpdateForm(instance=request.user.profile)
+    return render(request, "accounts/profile.html", {"form": form})
