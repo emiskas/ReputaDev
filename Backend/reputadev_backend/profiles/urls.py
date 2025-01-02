281a32f79cf7c1910d285e36fd01e86342bc7751
Emilis Paulauskas <emilispaulauskas279@gmail.com>
2025-01-02 15:27:37 +0200

    Make profile URL dynamic by using a username.

--- Backend/reputadev_backend/profiles/urls.py
+++ Backend/reputadev_backend/profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import profile_view
 
 urlpatterns = [
-    path("profile/", profile_view, name="profile"),
+    path("profile/<str:username>/", profile_view, name="profile"),
 ]
