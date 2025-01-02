65676ebc0441e0cbb7f8a6fdeab8d132d2fe8aed
Emilis Paulauskas <emilispaulauskas279@gmail.com>
2025-01-02 12:37:49 +0200

    Add a profile view to view the first profile in the DB.

--- Backend/reputadev_backend/profiles/views.py
+++ Backend/reputadev_backend/profiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+def profile_view(request):
+    profile = Profile.objects.first()
+
+    context = {
+        "profile": profile,
+    }
+
+    return render(request, "profile.html", context)
