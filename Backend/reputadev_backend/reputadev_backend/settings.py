d8cec8354ca8b09e102ffceab383a5b9d16dbc25
Emilis Paulauskas <emilispaulauskas279@gmail.com>
2025-01-06 21:45:17 +0200

    Add CORS headers into Django apps and allow frontend URL.

--- Backend/reputadev_backend/reputadev_backend/settings.py
+++ Backend/reputadev_backend/reputadev_backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "profiles",
     "accounts",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -55,6 +56,7 @@ LOGIN_REDIRECT_URL = "/"  # Redirect to home or dashboard after login
 LOGOUT_REDIRECT_URL = "/"  # Redirect to home or dashboard after logout
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -62,6 +64,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React frontend URL
 ]
 
 ROOT_URLCONF = "reputadev_backend.urls"
